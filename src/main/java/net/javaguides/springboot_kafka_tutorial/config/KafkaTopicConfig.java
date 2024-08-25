5de9b04b41e110cc533a51310e3c661016837903
Anshuman <verma77712@gmail.com>
2024-08-25 13:29:53 +0530

    first commit

---
+++ src/main/java/net/javaguides/springboot_kafka_tutorial/config/KafkaTopicConfig.java
@@ -0,0 +1,19 @@
+package net.javaguides.springboot_kafka_tutorial.config;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic javaguidesTopic(){
+        // Create a new topic instance
+        return TopicBuilder.name("javaguides").build();
+    }
+
+
+}
